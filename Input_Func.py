64ee0fcba21f43d64340ffdb734563a57c96b9a6
AbhijitSahani <88982270+AbhijitSahani@users.noreply.github.com>
2025-04-12 21:09:52 +0530

    Add files via upload

---
+++ Input_Func.py
@@ -0,0 +1,4 @@
+a=input("Enter 1st num :")
+b=input("Enter 2nd num :")
+c=a+b
+print("RESULT =",int(a) + int(b)\n"Proceed to another?")
---
+++ Main.py
@@ -0,0 +1,3 @@
+a=7
+b=8
+print("Addition of both Numbers" ,a, "+",b, "=",a+b)
